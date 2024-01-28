080d98adeb6f2efc43d5ea676ea1fb323384bbf5
Emily <emilysoolee@gmail.com>
2024-01-28 00:03:49 -0800

    added three buttons for three main functionalities

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-# Start your program and a GUI is generated
+# # Start your program and a GUI is generated
+# # https://www.pysimplegui.org/en/latest/
 
 import PySimpleGUI as sg
-import tkinter 
 
-layout = [[sg.Text("This program allow you to upload a single file and detects/refactors code smells")], [sg.Button("OK")]]
+sg.theme('BluePurple')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('This tool analyzes the code in your file and detects duplicated code. ')],
+            [sg.Button('Upload File'), sg.Button('Start Analysis'), sg.Button('Exit')] ]
 
-# Create the window
-window = sg.Window("Code Smell Detector and Refactoring Tool", layout)
+# Create the Window
+window = sg.Window('Code Smell Detector and Refactoring Tool', layout)
 
-
-# Users close the program through GUI 
-# Create an event loop
+# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == 'Upload File':
+        layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],[sg.Button("Submit")]]
+
+        #Building Window
+        window = sg.Window('My File Browser', layout, size=(600,150))
+    
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event=="Exit":
+                break
+            elif event == "Submit":
+                print(values["-IN-"])
+
+    if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks Exit
         break
+    print('You entered ', values[0])
 
 window.close()
-
           
