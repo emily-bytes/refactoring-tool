8d6c249be42d8458084a71b437238ac5fa43823d
Emily <emilysoolee@gmail.com>
2024-02-20 09:53:29 -0800

    added source code for testing

---
+++ codeSmells.py
@@ -0,0 +1,68 @@
+class Something: 
+    def do_something(n, t, z, p):
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")
+
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+        return 0
+
+    def do_something_again():
+        n, t = 10, 10
+
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")
+
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")    
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")
+        return 0
+    
+    def do_something_again():
+        n, t = 10, 10
+
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")
+
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")    
+        for i in range(1, n + 1):
+            if i == 1:
+                print(f"{n}, ", end="")
+
+        for i in range(1, t + 1):
+            if t == 1:
+                print(f"{t}, ", end="")
+        return 0    
