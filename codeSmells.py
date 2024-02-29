713f102dd82a6da57321e858dd52138d5dd254c4
Emily <emilysoolee@gmail.com>
2024-02-28 16:47:08 -0800

    updated testing file to reflect all three code smells

--- codeSmells.py
+++ codeSmells.py
@@ -1,19 +1,15 @@
-class Something: 
-    def do_something(n, t, z, p):
-        for i in range(1, n + 1):
-            if i == 1:
-                print(f"{n}, ", end="")
-
-        for i in range(1, t + 1):
-            if t == 1:
-                print(f"{t}, ", end="")
-
+class Tests: 
+    def not_long_method(n):
+        n = 3
         for i in range(1, n + 1):
             if i == 1:
                 print(f"{n}, ", end="")
         return 0
-
-    def do_something_again():
+    
+    def method(n):
+        return n 
+    
+    def is_long_method():
         n, t = 10, 10
 
         for i in range(1, n + 1):
@@ -40,29 +36,9 @@ class Something:
                 print(f"{t}, ", end="")
         return 0
     
-    def do_something_again():
-        n, t = 10, 10
-
-        for i in range(1, n + 1):
-            if i == 1:
-                print(f"{n}, ", end="")
-
-        for i in range(1, t + 1):
-            if t == 1:
-                print(f"{t}, ", end="")
-
-        for i in range(1, n + 1):
-            if i == 1:
-                print(f"{n}, ", end="")
-
-        for i in range(1, t + 1):
-            if t == 1:
-                print(f"{t}, ", end="")    
-        for i in range(1, n + 1):
-            if i == 1:
-                print(f"{n}, ", end="")
-
-        for i in range(1, t + 1):
-            if t == 1:
-                print(f"{t}, ", end="")
-        return 0    
+    def not_long_parameter_list(n=0, t=5):
+        y = n + t
+        return y    
+    
+    def long_parameter_list(n, t, y, z):
+        return n + t + y + z
